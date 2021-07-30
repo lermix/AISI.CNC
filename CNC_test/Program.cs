c4458ed98fb23cfd6885361ae360679680f97aac
Alen Muller <alen.muller@adnet.hr>
2021-07-30 09:20:16 +0200

    first commit

---
+++ CNC_test/Program.cs
@@ -0,0 +1,90 @@
+﻿using Sharer;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace CNC_test
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//var connection = new SharerConnection( "COM3", 115200 );
+			//connection.Connect();
+
+			byte[] fileBytes = File.ReadAllBytes( @"C:\Users\amuller\Documents\CNC\proba.STL" );
+			char[] header_info = new char[80];
+			ulong n_tri = new ulong();
+			byte[] nTriByte = new byte[4];
+			byte[] facet = new byte[50];
+
+			HashSet<stlVecor> list = new HashSet<stlVecor>();
+
+			for ( int i = 0; i < 80; i++ )
+			{
+				header_info[i] = Convert.ToChar( fileBytes[i] );
+				Console.Write( header_info[i] );
+			}
+			for ( int i = 80; i < 84; i++ )
+			{
+				nTriByte[i-80] = fileBytes[i];
+			}
+
+			n_tri = BitConverter.ToUInt32( nTriByte );
+			Console.WriteLine();
+
+			for ( int i = 0; i < Convert.ToInt32( n_tri); i++ )
+			{
+				int from = ( 84 + ( i * 50 ));
+				facet = subArray( fileBytes, from, from+50);
+
+				//Console.WriteLine( new stlVecor( subArray( facet, 0, 12 ) ) );
+				//Console.WriteLine( new stlVecor( subArray( facet, 12, 24 ) ) );
+				//Console.WriteLine( new stlVecor( subArray( facet, 24, 36 ) ) );
+
+				//list.Add( new stlVecor( subArray( facet, 0, 12 ) ) );
+				list.Add( new stlVecor( subArray( facet, 12, 24 ) ) );
+				list.Add( new stlVecor( subArray( facet, 24, 36 ) ) );
+				list.Add( new stlVecor( subArray( facet, 36, 48) ) );
+			}
+
+			int counter = 1;
+			foreach ( stlVecor item in list )
+			{
+				Console.WriteLine(counter + ". "+ item);
+				counter++;
+			}
+			Console.WriteLine();
+			Console.WriteLine("ntri:"+n_tri);
+
+			Console.WriteLine( "Finished reading" );
+			Console.ReadLine();
+
+			
+
+			/*while ( true )
+			{
+				Console.WriteLine("state:" + connection.ReadVariable( "myVar" ));
+				int a = int.Parse(Console.ReadKey().KeyChar.ToString());
+				Console.WriteLine("I enered: ", a);
+				connection.WriteVariable( "myVar", a );
+
+			}*/
+
+			
+
+		
+			
+		}
+
+		public static byte[] subArray(byte[] array, int from, int to)
+		{
+			byte[] returnValue = new byte[to-from];
+			for ( int i = from; i < to; i++ )
+			{
+				returnValue[i-from] = array[i];
+			}
+			return returnValue;
+		}
+	}
+}
---
+++ CNC_test/stlVecor.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CNC_test
+{
+	class stlVecor
+	{
+		public float x { get; set; }
+		public float y { get; set; }
+		public float z { get; set; }
+
+		
+
+		public stlVecor(byte[] facet)
+		{
+			byte[] f1 = { facet[0], facet[1], facet[2], facet[3]  };
+			byte[] f2 = { facet[4], facet[5], facet[6], facet[7] };
+			byte[] f3 = { facet[8], facet[9], facet[10], facet[11] };
+
+			x = BitConverter.ToSingle( f1,0 ) < 0.001 ? 0 : BitConverter.ToSingle( f1, 0 );
+			y = BitConverter.ToSingle( f2,0 ) < 0.001 ? 0 : BitConverter.ToSingle( f2, 0 );
+			z = BitConverter.ToSingle( f3,0 ) < 0.001 ? 0 : BitConverter.ToSingle( f3, 0 );
+		}
+
+		public static bool operator ==(stlVecor first, stlVecor second) => (first.x, first.y, first.z) == (second.x, second.y, second.z);
+
+		public static bool operator !=(stlVecor first, stlVecor second) => (first.x, first.y, first.z) != (second.x, second.y, second.z);
+
+
+
+		public override string ToString()
+		{
+			return "x: " + x + ", y: " + y + ", z: " + z;
+		}
+
+		public override bool Equals(object obj)
+		{
+			return obj is stlVecor vecor &&
+					 x == vecor.x &&
+					 y == vecor.y &&
+					 z == vecor.z;
+		}
+
+		public override int GetHashCode()
+		{
+			return HashCode.Combine( x, y, z );
+		}
+
+	}
+}
